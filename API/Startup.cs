5b76188dbb8e582b626213dd68c9c8ce534cf14e
Mosh Hamedanii <programmingwithmosh@gmail.com>
2022-03-15 15:29:23 +0100

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -31,6 +32,10 @@ namespace API
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+
+            services.AddAutoMapper(typeof(MappingProfiles));
             
         }
 
@@ -46,6 +51,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -24,6 +24,11 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
+         /*   var typeId = 1;
+
+            var products = _context.Products
+                .Where(x => x.ProductTypeId == typeId).Include(x => x.ProductType); */
+
             return await _context.Products
                 .Include(p => p.ProductType)
                 .Include(p => p.ProductBrand)
